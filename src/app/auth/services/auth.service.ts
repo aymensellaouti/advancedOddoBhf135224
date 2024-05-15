38daab96d6952a7126f846997e84a2f070d1250d
aymensellaouti <aymen.sellaouti@gmail.com>
2024-05-15 12:07:05 +0100

    end Auth + start Product

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -3,7 +3,7 @@ import { CredentialsDto } from '../dto/credentials.dto';
 import { LoginResponseDto } from '../dto/login-response.dto';
 import { HttpClient } from '@angular/common/http';
 import { API } from '../../../config/api.config';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, map, tap } from 'rxjs';
 
 export class AuthUser {
   constructor(public id = 0, public email = ''){}
@@ -16,18 +16,31 @@ export class AuthService {
   #userSubject = new BehaviorSubject<AuthUser | null>(null);
   user$ = this.#userSubject.asObservable();
   // Todo : Define loggedIn$ and loggedOut$
-  loggedIn$!: Observable<boolean>;
-  loggedOut$!: Observable<boolean>;
+  loggedIn$: Observable<boolean> = this.user$.pipe(map((user) => !!user));
+  loggedOut$: Observable<boolean> = this.user$.pipe(map((user) => !user));
   constructor(private http: HttpClient) {
     // Todo : vérifier est ce que le user existe fel localStorage
+    const user = localStorage.getItem('user');
     // si oui je le récupére et je déclenche le beahivourSubject
     // sinon rien
+    if (user) {
+      this.#userSubject.next(JSON.parse(user));
+    }
   }
 
   login(credentials: CredentialsDto): Observable<LoginResponseDto> {
-    // Todo save user fel localStorage
-    // Déclenchiw elsubject bel user authenticated
-    return this.http.post<LoginResponseDto>(API.login, credentials);
+    return this.http.post<LoginResponseDto>(API.login, credentials).pipe(
+      tap((response) => {
+        // Todo save user fel localStorage
+        // Nkhabi el token
+        localStorage.setItem('token', response.id);
+        //Nkhabi el user
+        const user = new AuthUser(response.userId, credentials.email);
+        localStorage.setItem('user', JSON.stringify(user));
+        // Déclenchiw elsubject bel user authenticated
+        this.#userSubject.next(user);
+      })
+    );
   }
 
   isAuthenticated(): boolean {
@@ -37,6 +50,8 @@ export class AuthService {
   logout() {
     // Todo : Je vide le local storage mel user
     // declanchi null fel behaviour subject
+    localStorage.removeItem('user');
     localStorage.removeItem('token');
+    this.#userSubject.next(null);
   }
 }
