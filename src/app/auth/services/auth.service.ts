195c9fffcaaf817c342e7f01032d572d95af643f
aymensellaouti <aymen.sellaouti@gmail.com>
2024-05-15 11:07:13 +0100

    init auth

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -3,15 +3,30 @@ import { CredentialsDto } from '../dto/credentials.dto';
 import { LoginResponseDto } from '../dto/login-response.dto';
 import { HttpClient } from '@angular/common/http';
 import { API } from '../../../config/api.config';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+export class AuthUser {
+  constructor(public id = 0, public email = ''){}
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  #userSubject = new BehaviorSubject<AuthUser | null>(null);
+  user$ = this.#userSubject.asObservable();
+  // Todo : Define loggedIn$ and loggedOut$
+  loggedIn$!: Observable<boolean>;
+  loggedOut$!: Observable<boolean>;
+  constructor(private http: HttpClient) {
+    // Todo : vérifier est ce que le user existe fel localStorage
+    // si oui je le récupére et je déclenche le beahivourSubject
+    // sinon rien
+  }
 
   login(credentials: CredentialsDto): Observable<LoginResponseDto> {
+    // Todo save user fel localStorage
+    // Déclenchiw elsubject bel user authenticated
     return this.http.post<LoginResponseDto>(API.login, credentials);
   }
 
@@ -20,6 +35,8 @@ export class AuthService {
   }
 
   logout() {
+    // Todo : Je vide le local storage mel user
+    // declanchi null fel behaviour subject
     localStorage.removeItem('token');
   }
 }
