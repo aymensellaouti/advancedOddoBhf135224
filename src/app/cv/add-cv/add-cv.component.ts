bfbd43e70b4d0f8a1e41c83c33eb0dd5b80b5cdf
aymensellaouti <aymen.sellaouti@gmail.com>
2024-05-16 09:07:10 +0100

    add Cv Mise en place de Reactive Form

--- src/app/cv/add-cv/add-cv.component.ts
+++ src/app/cv/add-cv/add-cv.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
 import { CvService } from '../services/cv.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
@@ -12,17 +12,27 @@ import { Cv } from '../model/cv';
   styleUrls: ['./add-cv.component.css'],
 })
 export class AddCvComponent {
+  formBuilder = inject(FormBuilder);
+  form: FormGroup = this.formBuilder.group({
+    name: [''],
+    firstname: [''],
+    job: [''],
+    path: [''],
+    age: [40],
+    cin: [''],
+  });
+
   constructor(
     private cvService: CvService,
     private router: Router,
     private toaster: ToastrService
   ) {}
 
-  addCv(cv: Cv) {
-    this.cvService.addCv(cv).subscribe({
+  addCv() {
+    this.cvService.addCv(this.form.value)
+    .subscribe({
       next: () => {
-        this.toaster
-          .success(`Le cv a été ajouté avec succès`);
+        this.toaster.success(`Le cv a été ajouté avec succès`);
         this.router.navigate([APP_ROUTES.cv]);
       },
       error: (erreur) => {
@@ -32,5 +42,24 @@ export class AddCvComponent {
         );
       },
     });
+  }
+
+  get name() {
+    return this.form.get('name')!;
+  }
+  get firstname() {
+    return this.form.get('firstname')!;
+  }
+  get age() {
+    return this.form.get('age')!;
+  }
+  get path() {
+    return this.form.get('path')!;
+  }
+  get cin() {
+    return this.form.get('cin')!;
+  }
+  get job() {
+    return this.form.get('job')!;
   }
 }
